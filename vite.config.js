b3e98dfc7eee97919e7df27b660474e1e7d1b0f3
maruchoatomoshi <@.com>
2026-03-29 22:40:26 +0300

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/zhidao-react/',
+  base: '/zhidao-react/',  // ← Вот здесь имя репозитория
   server: {
     proxy: {
       '/api': {
